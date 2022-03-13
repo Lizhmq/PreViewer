b932a6c063d59b4833463a7e2a177753508c0b2c
Joe Lee <lizhmq@pku.edu.cn>
2022-03-13 12:17:11 +0000

    update demo bleu calculator.

--- code/bleu.py
+++ code/bleu.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 from evaluator.smooth_bleu import bleu_fromstr
+import nltk
+import re
 
 
 def main():
@@ -17,12 +19,19 @@ def main():
     hyps = [hyp.strip().lower() for hyp in hyps]
     bleu = bleu_fromstr(hyps, refs)
     print(bleu)
-    stopwords = open("stopwords.txt").readlines()
-    stopwords = [stopword.strip() for stopword in stopwords]
-    refs = [" ".join([word for word in ref.lower().split() if word not in stopwords]) for ref in refs]
-    hyps = [" ".join([word for word in hyp.lower().split() if word not in stopwords]) for hyp in hyps]
-    bleu = bleu_fromstr(hyps, refs)
+    bleu = bleu_fromstr(hyps, refs, rmstop=False)
     print(bleu)
+    # stopwords = open("stopwords.txt").readlines()
+    # stopwords = [stopword.strip() for stopword in stopwords]
+    # refs = [" ".join([word for word in ref.lower().split() if word not in stopwords]) for ref in refs]
+    # hyps = [" ".join([word for word in hyp.lower().split() if word not in stopwords]) for hyp in hyps]
+    # bleu = bleu_fromstr(hyps, refs)
+    # print(bleu)
 
 if __name__ == '__main__':
-    main()
+    # main()
+    s = "Can we use `mset.mirrorInfo()` directly?"
+    chars = "(_)`."
+    for c in chars:
+        s = s.replace(c, " " + c + " ")
+    print(nltk.wordpunct_tokenize(s))
